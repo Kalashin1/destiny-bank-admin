c64bfabad85bb5b890b39010e1d989bb4235c37b
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2025-11-24 19:00:24 +0100

    added more UI components

---
+++ src/pages/dashboard/components/filter-transaction-table.tsx
@@ -0,0 +1,165 @@
+const FilterTransactionTable = () => {
+  return (
+    <div className="ac-panel">
+      <div className="max-w-2xl py-3">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-5 lg:gap-6">
+          <label className="block">
+            <span>Employer name:</span>
+            <div className="relative mt-1.5 flex">
+              <input
+                className="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                placeholder="Enter Employer Name"
+                type="text"
+              />
+              <span className="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4.5 w-4.5 transition-colors duration-200"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    d="M5 19.111c0-2.413 1.697-4.468 4.004-4.848l.208-.035a17.134 17.134 0 015.576 0l.208.035c2.307.38 4.004 2.435 4.004 4.848C19 20.154 18.181 21 17.172 21H6.828C5.818 21 5 20.154 5 19.111zM16.083 6.938c0 2.174-1.828 3.937-4.083 3.937S7.917 9.112 7.917 6.937C7.917 4.764 9.745 3 12 3s4.083 1.763 4.083 3.938z"
+                  />
+                </svg>
+              </span>
+            </div>
+          </label>
+          <label className="block">
+            <span>Project name:</span>
+            <div className="relative mt-1.5 flex">
+              <input
+                className="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                placeholder="Enter Project Name"
+                type="text"
+              />
+              <span className="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4.5 w-4.5 transition-colors duration-200"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    d="M3.082 13.944c-.529-.95-.793-1.425-.793-1.944 0-.519.264-.994.793-1.944L4.43 7.63l1.426-2.381c.559-.933.838-1.4 1.287-1.66.45-.259.993-.267 2.08-.285L12 3.26l2.775.044c1.088.018 1.631.026 2.08.286.45.26.73.726 1.288 1.659L19.57 7.63l1.35 2.426c.528.95.792 1.425.792 1.944 0 .519-.264.994-.793 1.944L19.57 16.37l-1.426 2.381c-.559.933-.838 1.4-1.287 1.66-.45.259-.993.267-2.08.285L12 20.74l-2.775-.044c-1.088-.018-1.631-.026-2.08-.286-.45-.26-.73-.726-1.288-1.659L4.43 16.37l-1.35-2.426z"
+                  />
+                  <circle
+                    cx="12"
+                    cy="12"
+                    r="3"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                  />
+                </svg>
+              </span>
+            </div>
+          </label>
+          <label className="block">
+            <span>From:</span>
+            <div className="relative mt-1.5 flex">
+              <input
+                id="filter-from-datepicker"
+                className="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                placeholder="Choose start date..."
+                type="text"
+              />
+              <span className="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 transition-colors duration-200"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+              </span>
+            </div>
+          </label>
+          <label className="block">
+            <span>To:</span>
+            <div className="relative mt-1.5 flex">
+              <input
+                id="filter-to-datepicker"
+                className="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                placeholder="Choose start date..."
+                type="text"
+              />
+              <div className="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 transition-colors duration-200"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </label>
+          <div className="sm:col-span-2">
+            <span>Project Status:</span>
+            <div className="mt-2 grid grid-cols-1 gap-4 sm:grid-cols-4 sm:gap-5 lg:gap-6">
+              <label className="inline-flex items-center space-x-2">
+                <input
+                  className="form-checkbox is-basic h-5 w-5 rounded border-slate-400/70 checked:border-secondary checked:bg-secondary hover:border-secondary focus:border-secondary dark:border-navy-400 dark:checked:border-secondary-light dark:checked:bg-secondary-light dark:hover:border-secondary-light dark:focus:border-secondary-light"
+                  type="checkbox"
+                />
+                <span>Upcoming</span>
+              </label>
+              <label className="inline-flex items-center space-x-2">
+                <input
+                  className="form-checkbox is-basic h-5 w-5 rounded border-slate-400/70 checked:border-primary checked:bg-primary hover:border-primary focus:border-primary dark:border-navy-400 dark:checked:border-accent dark:checked:bg-accent dark:hover:border-accent dark:focus:border-accent"
+                  type="checkbox"
+                />
+                <span>In Progress</span>
+              </label>
+              <label className="inline-flex items-center space-x-2">
+                <input
+                  checked
+                  className="form-checkbox is-basic h-5 w-5 rounded border-slate-400/70 checked:border-success! checked:bg-success hover:border-success! focus:border-success! dark:border-navy-400"
+                  type="checkbox"
+                />
+                <span>Complete</span>
+              </label>
+              <label className="inline-flex items-center space-x-2">
+                <input
+                  checked
+                  className="form-checkbox is-basic h-5 w-5 rounded border-slate-400/70 checked:border-error! checked:bg-error hover:border-error! focus:border-error! dark:border-navy-400"
+                  type="checkbox"
+                />
+                <span>Cancelled</span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="mt-4 space-x-1 text-right">
+          <button className="btn font-medium text-slate-700 hover:bg-slate-300/20 active:bg-slate-300/25 dark:text-navy-100 dark:hover:bg-navy-300/20 dark:active:bg-navy-300/25">
+            Cancel
+          </button>
+
+          <button className="btn bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
+            Apply
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FilterTransactionTable;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,12 @@
 import DashboardCard from "./components/dashboard-card";
+import TransactionTable from "./components/transaction-table";
 import Layout from "./layout";
 
 const Dashboard = () => {
   return (
     <Layout>
       <DashboardCard />
+      <TransactionTable />
     </Layout>
   );
 };
