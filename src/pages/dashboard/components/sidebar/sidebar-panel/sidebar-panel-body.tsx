1624e780914ea2b79e0d1e44847f6338681f0b1f
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2025-11-28 23:01:59 +0100

    added add_balance link

--- src/pages/dashboard/components/sidebar/sidebar-panel/sidebar-panel-body.tsx
+++ src/pages/dashboard/components/sidebar/sidebar-panel/sidebar-panel-body.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 import SCREENS from "../../../../../navigation/constants";
+import { auth } from "../../../../../firebase-settings";
 
 const SidebarPanelBody = () => {
+  const currentUser = auth.currentUser;
   const links = [
     {
       text: "Dashboard",
       link: SCREENS.DASHBOARD,
+    },
+    {
+      text: "Add Balance",
+      link: SCREENS.ADD_BALANCE,
     },
     {
       text: "Transactions",
@@ -30,18 +36,26 @@ const SidebarPanelBody = () => {
       data-simplebar
     >
       <ul className="flex flex-1 flex-col px-4 font-inter">
-        {links.map((link, index) => (
-          <li key={index}>
-            <Link
-              to={link.link}
-              data-default-class="text-slate-600 hover:text-slate-800 dark:text-navy-200 dark:hover:text-navy-50"
-              data-active-class="font-medium text-primary dark:text-accent-light"
-              className="nav-link flex py-2 text-xs+ tracking-wide outline-none transition-colors duration-300 ease-in-out"
-            >
-              {link.text}
-            </Link>
-          </li>
-        ))}
+        {links.map((link, index) => {
+          if (
+            link.link === SCREENS.ADD_BALANCE &&
+            !currentUser?.email?.includes("@admin")
+          )
+            return;
+          else
+            return (
+              <li key={index}>
+                <Link
+                  to={link.link}
+                  data-default-class="text-slate-600 hover:text-slate-800 dark:text-navy-200 dark:hover:text-navy-50"
+                  data-active-class="font-medium text-primary dark:text-accent-light"
+                  className="nav-link flex py-2 text-xs+ tracking-wide outline-none transition-colors duration-300 ease-in-out"
+                >
+                  {link.text}
+                </Link>
+              </li>
+            );
+        })}
       </ul>
     </div>
   );
